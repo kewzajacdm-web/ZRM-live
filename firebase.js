5450943346f09bf658b08124264173ef969476b0
kewzajacdm-web <kew.zajacdm@gmail.com>
2026-07-21 09:59:39 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "checklista-d39c0.firebaseapp.com",
+  projectId: "checklista-d39c0",
+  storageBucket: "checklista-d39c0.firebasestorage.app",
+  messagingSenderId: "1077383270775",
+  appId: "TU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
